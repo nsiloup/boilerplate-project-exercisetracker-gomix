ffc49013680d8e4404d0af33facb4cb81a6d6331
NSILOU BALANDAMIO perseverance <nsiloup@gmail.com>
2022-03-25 23:26:31 +0100

    Mission complete

--- controllers/logsControllers.js
+++ controllers/logsControllers.js
@@ -9,15 +9,50 @@ let logsModel_create_logs = async(req, res, next)=>{
         let o = await UsersModel.findById(_id, {username :1}).lean(); // The object for "users" items
         let username = o.username;
 
-    let exercisesArr = await ExercisesModel.find({globalUserId : _id}, {_id:0, _num : 0, globalUserId : 0, username : 0, __v : 0, /*description : 1, duration : 1, date : 1*/}).lean();
-    log("exercisesArr : ", exercisesArr)
+        let exercisesArr = await ExercisesModel.find({globalUserId : _id}, {_id:0, _num : 0, globalUserId : 0, username : 0, __v : 0, /*description : 1, duration : 1, date : 1*/}).lean();
+
+        log("exercisesArr : ", exercisesArr)
         let count = exercisesArr.length;
 
         let obj = {};
         obj._id = _id;
         obj.username = username;
+
+        let unixNum = (date) =>{
+            return Number(new Date(date));
+        };
+
+        // Sort the array ascending by date
+        exercisesArr.sort((prev, curr)=>{
+            return ( unixNum(prev.date) - unixNum(curr.date));
+        })
+
+
+        // "from", "to", "limit" PARAMETERS WILL GO HERE
+        let slicedArr = exercisesArr;
+        if(from && to){
+            obj.from = from;
+            obj.to = to;
+            //FINDING THE INDEX at where to slice the returned array
+            let fromInd = exercisesArr.findIndex(elt => unixNum(elt.date) >= unixNum(from));
+            let copyArr = exercisesArr.slice();
+            let toInd = copyArr.reverse().findIndex(elt => unixNum(elt.date) <= unixNum(to));
+            
+            let start;
+            fromInd === -1 ? start = 0 : start = fromInd;// If no near "from" was found
+
+            let end;
+            toInd  === - 1 ? end = exercisesArr.length : end = exercisesArr.length - toInd; //if no near "toInd" was found
+            log("fromInd",start, '\n',"toInd",end);
+            slicedArr = exercisesArr.slice(start, end)
+        };
+
+        if(limit){
+            slicedArr = slicedArr.slice(0, limit);
+        }
+
         obj.count = count;
-        obj.log = exercisesArr;
+        obj.log = slicedArr;
 
         res.json(obj);
         
--- helpers/deleteSome.js
+++ helpers/deleteSome.js
@@ -2,11 +2,11 @@ let {ExercisesModel} = require("../model/exercisesModel");
 let {UsersModel} = require("../model/usersModel");
 let log  = console.log;
 
-//Limit to 9 users, by deleting the exceding everytime the programme restart
+//Limit to 10 users, by deleting the exceding everytime the programme restart
 async function deleteSome(){
     try{
-        let deletedUsers = UsersModel.find({}, {_num : 1}).deleteMany({ _num: { $gte: 101 }});
-        let deletedExercises = ExercisesModel.find({}, {_num : 1}).deleteMany({ _num: { $gte: 101 }});
+        let deletedUsers = UsersModel.find({}, {_num : 1}).deleteMany({ _num: { $gte: 11 }});
+        let deletedExercises = ExercisesModel.find({}, {_num : 1}).deleteMany({ _num: { $gte: 11 }});
         let deleteSelcted = await Promise.all([
             deletedUsers, deletedExercises,
         ]);
